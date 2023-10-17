19b1da711096731336ea11eae21ae5fcbf3a5e34
Rahul Sharma <rs8290093612@gmail.com>
2023-10-17 13:30:07 +0530

    hy

--- about.php
+++ about.php
@@ -1,2 +1,40 @@
-fdldkjslkjlkfjslkajlfkajl;kaj;lkasdjlaksjaslk;djalksj
-ejfnejfjnrejnr
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>About Our College</title>
+    <link rel="stylesheet" type="text/css" href="../Css/About.css">
+</head>
+<body>
+    <header class="header">
+        <h1>About Our College</h1>
+    </header>
+    
+    <section class="section">
+        <h2>College Information</h2>
+        <p class="paragraph">
+            Insert a brief description of your college here. Include details like location, history, mission, and values.
+        </p>
+    </section>
+    
+    <section class="section">
+        <h2>Our Instructors</h2>
+        <ul class="list">
+            <li>
+                <h3>Instructor 1</h3>
+                <p class="paragraph">Provide information about the first instructor, such as their name, qualifications, and a short bio.</p>
+            </li>
+            <li>
+                <h3>Instructor 2</h3>
+                <p class="paragraph">Include details about the second instructor here.</p>
+            </li>
+        </ul>
+    </section>
+    
+    <!-- <button class="button" type="submit">Send Message</button> -->
+</body>
+</html>
